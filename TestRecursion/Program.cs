1f82de824fc9bed2dbe7cdf934b61e5fb4d3430b
kamel-rush <kamel@grandcircus.co>
2016-10-27 11:30:44 -0400

    Initial commit

---
+++ TestRecursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(5));
+        }
+
+
+        static int Fact(int x)
+        {  // if (x == 1)
+              //  return 1;
+         
+            return x * Fact(x - 1);
+
+        }
+    }
+}
